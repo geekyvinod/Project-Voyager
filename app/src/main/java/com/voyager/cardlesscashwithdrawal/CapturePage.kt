95a3fb783bee456833d0a4e71520f21287521b0d
vinod <vbshirsikar@gmail.com>
2020-07-19 12:34:30 +0530

    Added multiple activities

---
+++ app/src/main/java/com/voyager/cardlesscashwithdrawal/CapturePage.kt
@@ -0,0 +1,11 @@
+package com.voyager.cardlesscashwithdrawal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture_page)
+    }
+}
--- app/src/main/java/com/voyager/cardlesscashwithdrawal/ReviewPage.kt
+++ app/src/main/java/com/voyager/cardlesscashwithdrawal/ReviewPage.kt
@@ -3,9 +3,9 @@ package com.voyager.cardlesscashwithdrawal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ReviewPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_review_page)
     }
 }
